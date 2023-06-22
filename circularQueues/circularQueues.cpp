26c069c2513233c49498cf971ca77f7ebc676033
muhfarrasm <muhammadfarrasm@gmail.com>
2023-06-22 11:11:55 +0700

    Membuat fungsi loop while  Iterasi dari awal aray hingga rear

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -85,6 +85,15 @@ public:
 				cout << queue_array[FRONT_position] << " ";
 				FRONT_position++;
 			}
+
+			FRONT_position = 0;
+
+			//Iterasi dari awal aray hingga rear
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << " ";
+				FRONT_position++;
+			}
+			cout << endl;
 		}
 		
 	}
