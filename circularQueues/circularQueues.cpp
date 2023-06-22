e3d5e09bec56a69c9ba934542d729ad6c1671310
muhfarrasm <muhammadfarrasm@gmail.com>
2023-06-22 11:07:01 +0700

    Membuat fungsi  FRONT_position > REAR_position, iterasi dari  FRONT hingga REAR

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -70,6 +70,14 @@ public:
 			return;
 		}
 		cout << "\nElement di queue : \n";
+
+		// Jika FRONT_position > REAR_position, iterasi dari  FRONT hingga REAR	
+		if (FRONT_position <= REAR_position) {
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << " ";
+				FRONT_position++;
+			}
+		}
 		
 	}
 
