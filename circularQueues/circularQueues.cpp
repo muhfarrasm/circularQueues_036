ddbe59841a8d67e6c750978688c59921b5ce64a0
muhfarrasm <muhammadfarrasm@gmail.com>
2023-06-22 10:38:34 +0700

    Membuat fungsi insert

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -11,4 +11,18 @@ public:
 		REAR = -1;
 	}
 
+	void insert() {
+		int num;
+		cout << "Enter a Number: ";
+		cin >> num;
+		cout << endl;
+
+		//cek apakah antrian penuh
+		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
+			cout << "\nQueue overlow\n";
+			return;
+		}
+	}
+
+
 };
